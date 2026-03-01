ad98ce489aa6a6e7c42d1bf0f4bee6330f942bd5
fbk <fbk.fazekas@gmail.com>
2026-03-01 20:35:17 +0100

    ticket

--- JegyMester.DataContext/Entities/Ticket.cs
+++ JegyMester.DataContext/Entities/Ticket.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using JegyMester.DataContext.Enums;
+using System;
 
 namespace JegyMester.DataContext.Entities
 {
     public class Ticket
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TicketType Type { get; set; }
+        public bool valid { get; set; }
+        public int Price { get; set; }
     }
 }
