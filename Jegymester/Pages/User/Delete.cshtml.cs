f9daed8b2fc4f9304ecd5725df88087d9c8c7807
KSandor2003 <147424430+KSandor2003@users.noreply.github.com>
2026-04-05 15:41:29 +0200

    Hozzáadtam módosításokat

---
+++ Jegymester/Pages/User/Delete.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using JegyMester.DataContext.Context;
+
+namespace JegyMester.Pages.User
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly JegyMester.DataContext.Context.JegyMesterDbContext _context;
+
+        public DeleteModel(JegyMester.DataContext.Context.JegyMesterDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public DataContext.Entities.User User { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (user is not null)
+            {
+                User = user;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                User = user;
+                _context.Users.Remove(User);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
