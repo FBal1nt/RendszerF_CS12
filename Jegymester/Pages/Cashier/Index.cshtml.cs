60bfc5ff12f189a559e3eb6192ae586630d11a22
KSandor2003 <147424430+KSandor2003@users.noreply.github.com>
2026-04-17 21:29:49 +0200

    Megvan a Cashier, a pénztáros, és elvileg működnie kellene a leírás szerint

---
+++ Jegymester/Pages/Cashier/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JegyMester.Pages.Cashier
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Jegymester/Pages/Cashier/Sell/Edit.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using JegyMester.DataContext.Context;
+using JegyMester.DataContext.Entities;
+
+namespace JegyMester.Pages.Cashier.Sell_ticket
+{
+    public class EditModel : PageModel
+    {
+        private readonly JegyMester.DataContext.Context.JegyMesterDbContext _context;
+
+        public EditModel(JegyMester.DataContext.Context.JegyMesterDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public TicketPurchase TicketPurchase { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var purchase = await _context.TicketPurchases.FindAsync(id);
+            if (purchase == null) return NotFound();
+
+            TicketPurchase = purchase;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            _context.Attach(TicketPurchase).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TicketPurchaseExists(TicketPurchase.Id)) return NotFound();
+                throw;
+            }
+
+            return RedirectToPage("./Details", new { id = TicketPurchase.Id });
+        }
+
+        private bool TicketPurchaseExists(int id)
+        {
+            return _context.TicketPurchases.Any(e => e.Id == id);
+        }
+    }
+}
